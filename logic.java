54277ef9c61933f5d633612c06b58a045887fcff
erick <33715848+mainaerick@users.noreply.github.com>
2021-06-20 22:34:37 +0300

    Update logic.java

--- logic.java
+++ logic.java
@@ -11,15 +11,16 @@ import java.util.List;
 
 class logic {
     public static void main(String[] args) throws ParseException {
-        String[] time = { "2014-11-25 07:00", "2014-11-25 11:00", "2014-11-25 14:00", "2014-11-25 16:30" };
-        String[] totime = { "2014-11-25 09:00", "2014-11-25 13:00", "2014-11-25 16:00", "2014-11-25 18:00" };
+        String day = "2014-11-25 ";
+        String[] start_time = { "07:00", "11:00", "14:00", "16:30" };
+        String[] totime = { "09:00", "13:00", "16:00", "18:00" };
 
         DateTimeFormatter formatter;
         LocalDateTime localDateTime_day_to;
         LocalDateTime localDateTime_day_from;
-        Duration day_diff=null;
+        Duration day_diff = null;
         long diff_mili = 0;
-        String day_diff_string=null;
+        String day_diff_string = null;
 
         List<Date> intervals = new ArrayList<>(25);
         String day_from = "2014-11-25 06:00", day_to = "2014-11-25 20:00";// the start of the day to the end of the day
@@ -28,8 +29,8 @@ class logic {
         DateFormat format2 = new SimpleDateFormat("hh:mm a");
         Date start_time_day = format.parse(day_from);// convaert day start to date
         Date end_time_day = format.parse(day_to); // convert dya end to date
-        Date first_task_date=format.parse(time[0]);
-        
+        Date first_task_date = format.parse(day + start_time[0]);
+
         String first_task_date_string = format2.format(first_task_date); // convert date day start to string
         String setd2 = format.format(end_time_day); // convert date day end to string
 
@@ -38,20 +39,18 @@ class logic {
 
         intervals.add(start_time_day); // add date start day to list intervals
 
-        Calendar cal = Calendar.getInstance(),cal2=Calendar.getInstance();
+        Calendar cal = Calendar.getInstance(), cal2 = Calendar.getInstance();
         cal.setTime(start_time_day);
-        Date first_task_time = format.parse(time[0]);
+        Date first_task_time = format.parse(day + start_time[0]);
         cal2.setTime(first_task_time);
-         
 
-        // if day start ie 7:00 am is less than first task time 8:00, have a free time slot of one hour from 7-8
-        if(cal.before(cal2)){
+        // if day start ie 7:00 am is less than first task time 8:00, have a free time
+        // slot of one hour from 7-8
+        if (cal.before(cal2)) {
             String first_task_time_string = format2.format(start_time_day);
 
             System.out.println("free time slots " + first_task_time_string + " - " + first_task_date_string);
         }
-
-
 
         while (cal.getTime().before(end_time_day)) { // loop through every minute form day start time to day end time
                                                      // and add it to the list intervals
@@ -60,9 +59,9 @@ class logic {
             intervals.add(cal.getTime());
         }
 
-        for (int i = 0; i < time.length; i++) {
+        for (int i = 0; i < start_time.length; i++) {
 
-            Date date2 = format.parse(totime[i]);
+            Date date2 = format.parse(day + totime[i]);
 
             String totime_string = format2.format(date2);
 
@@ -70,12 +69,12 @@ class logic {
 
                 if (format2.format(intervals.get(j)).equals(totime_string)) {
                     Date date;
-                    if (i + 1 == time.length) {
+                    if (i + 1 == start_time.length) {
                         date = format.parse(setd2); // if its last time in the array set the date to end day time
 
                     } else {
 
-                        date = format.parse(time[i + 1]); // else set date to next task start time
+                        date = format.parse(day + start_time[i + 1]); // else set date to next task start time
                     }
 
                     String time_string = format2.format(date);
@@ -94,19 +93,19 @@ class logic {
 
             day_diff = Duration.between(localDateTime_day_from, localDateTime_day_to); // duration for the whole day
 
-
             day_diff_string = String.format("%d hour(s) %02d minutes", day_diff.toHoursPart(),
                     day_diff.toMinutesPart()); // whole day duration format to string
 
             try {
 
-                LocalDateTime lDateTime1 = LocalDateTime.parse(time[i], formatter); // from time localdatetime
-                LocalDateTime lDateTime2 = LocalDateTime.parse(totime[i], formatter); // to time local date time
+                LocalDateTime lDateTime1 = LocalDateTime.parse(day + start_time[i], formatter); // from time
+                                                                                                // localdatetime
+                LocalDateTime lDateTime2 = LocalDateTime.parse(day + totime[i], formatter); // to time local date time
 
                 Duration diff = Duration.between(lDateTime1, lDateTime2); // duration between task from time and task to
                                                                           // time
                 long temp = diff.toMillis(); // temporary value to hold diff converted to milliseconds
-                diff_mili=temp+diff_mili; // sum of all tasks duration for the day
+                diff_mili = temp + diff_mili; // sum of all tasks duration for the day
 
                 String format_duration_string = String.format("%d hour(s) %02d minutes", diff.toHoursPart(),
                         diff.toMinutesPart());
@@ -123,7 +122,7 @@ class logic {
 
         Duration ddmili_duration = Duration.ofMillis(diff_mili); // milliseconds to duration
         String format_duration_string = String.format("%d hour(s) %02d minutes", ddmili_duration.toHoursPart(),
-                        ddmili_duration.toMinutesPart());
+                ddmili_duration.toMinutesPart());
         System.out.println("sum of time taken by tasks is " + format_duration_string);
 
         long day_diff_mili = day_diff.toMillis() - ddmili_duration.toMillis();
